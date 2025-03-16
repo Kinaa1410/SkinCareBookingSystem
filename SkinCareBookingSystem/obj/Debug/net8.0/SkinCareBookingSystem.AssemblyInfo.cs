88fe87ccb0453238e3b253cb6f893cacd0f86b86
Kinaa1410 <120170157+Kinaa1410@users.noreply.github.com>
2025-03-16 22:11:24 +0700

    fix code

--- SkinCareBookingSystem/obj/Debug/net8.0/SkinCareBookingSystem.AssemblyInfo.cs
+++ SkinCareBookingSystem/obj/Debug/net8.0/SkinCareBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d4a781bc9a5bef2ebee1667f058fca53cb7fa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b4da3b73cd9556ec42782782172d24d0eba943")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
