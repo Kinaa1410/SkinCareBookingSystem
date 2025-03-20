8517d60a0bc8d31374321c8db5a2e6c4a02b847a
baodev <khanglam707@gmail.com>
2025-03-20 22:49:24 +0700

    New Code

--- SkinCareBookingSystem/obj/Debug/net8.0/SkinCareBookingSystem.AssemblyInfo.cs
+++ SkinCareBookingSystem/obj/Debug/net8.0/SkinCareBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c0cf74b5cdb1ecfb315c678307fe19c39cceb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c537b6be60adec1342b785fcb146af6764d1ba8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
