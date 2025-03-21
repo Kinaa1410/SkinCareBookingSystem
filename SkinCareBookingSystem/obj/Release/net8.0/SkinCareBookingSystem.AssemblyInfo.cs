4728175967345a6fb17c2572cb605cd6a0459ed1
baodev <khanglam707@gmail.com>
2025-03-21 22:48:35 +0700

    Fix API

--- SkinCareBookingSystem/obj/Release/net8.0/SkinCareBookingSystem.AssemblyInfo.cs
+++ SkinCareBookingSystem/obj/Release/net8.0/SkinCareBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8517d60a0bc8d31374321c8db5a2e6c4a02b847a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200a7bebb63ab1ec869453bd8128eb0a93b56eca")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkinCareBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
